04b251cba677e5054f1d4686a9232e2e94d126de
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-11-14 18:53:55 -0800

    Added code

--- citiesFeed/ContentView.swift
+++ citiesFeed/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Cities").font(.largeTitle)
+            ScrollView {
+                VStack {
+                    Card(imageName: "paris", cityName: "Paris")
+                    Card(imageName: "la", cityName: "Los Angeles")
+                    Card(imageName: "newyork", cityName: "New York")
+                    Card(imageName: "london", cityName: "London")
+                    Card(imageName: "rome", cityName: "Rome")
+                }.padding(.horizontal)
+            }
+        }
+        
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Card: View {
+    @State var imageName : String
+    @State var cityName : String
+    var body: some View {
+        ZStack {
+            Image(imageName).resizable().aspectRatio(contentMode: .fit)
+                .overlay(
+                    Rectangle().opacity(0.2)
+            )
+            Text(cityName).font(.system(size: 60, weight: .black, design: .rounded)).foregroundColor(.white)
+        }.cornerRadius(40)
     }
 }
